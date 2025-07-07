5308269331ab70293a8b51fb96905eff51bd849e
TK <Taylorannekaufman@gmail.com>
2025-07-07 14:32:00 -0400

    Updated python code comment about csv files

--- Bike_Share_Project.py
+++ Bike_Share_Project.py
@@ -6,6 +6,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+#Make sure the csv files are in the same location of the python program
+
 def bicycle_song():
     """
     Easter egg function that plays the Bicycle song lyrics.
